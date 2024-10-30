347c0f9ee9ad827ac510ee5c44097ca3b9768a3e
Teja99860 <teja.m@ifocussystec.in>
2024-10-30 19:34:13 +0530

    test

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Search from "@/app/ui/search";
-import { lusitana } from "../font";
+import { lusitana } from "../fonts";
 import {
   CustomersTableType,
   FormattedCustomersTable,
